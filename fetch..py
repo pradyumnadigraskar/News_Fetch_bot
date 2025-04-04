b0b876710677a850b1faef04cf749cdf097a0643
Pradyumna Digraskar <150524696+pradyumnadigraskar@users.noreply.github.com>
2025-04-04 13:01:41 +0530

    Update fetch..py

--- fetch..py
+++ fetch..py
@@ -1,10 +1,1 @@
-import requests
 
-API_KEY = "e1a2966772e047a886c65bcc645ab4ca"  
-URL = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}"
-
-response = requests.get(URL)
-data = response.json()
-
-for article in data['articles']:
-    print(article['title'], "-", article['url'])
